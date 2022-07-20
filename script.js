1986b0db4758663bc1ae0f865f8268b006008bbc
Matheus <matheusaraujo.lopes02092016@gmail.com>
2022-07-20 19:27:48 -0300

    exercicio 4

--- script.js
+++ script.js
@@ -44,6 +44,16 @@ if (one > two && one > three) {
 
 // 4- Faça um programa que, dado um valor definido numa constante, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.
 
+let numeroUm = -1;
+
+if (numeroUm < 0) {
+    console.log('Negative');
+} else if (numeroUm > 0) {
+    console.log('Positive');
+} else {
+    console.log('Zero');
+}
+
 //5- 🚀 Faça um programa que defina três constantes com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false, caso contrário. Se algum ângulo for inválido o programa deve retornar uma mensagem de erro.
 
 //6- Escreva um programa que receba o nome de uma peça de xadrez e retorne os movimentos que ela faz.
