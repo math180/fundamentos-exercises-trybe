8e0887addb7df8d2ca0622571b63543aa27a5c20
Matheus <matheusaraujo.lopes02092016@gmail.com>
2022-07-21 19:16:46 -0300

    exercicio 9

--- script.js
+++ script.js
@@ -84,3 +84,11 @@ for (let i = 0; i <= 25; i += 1) {
     array.push(i)
 } 
     console.log(array);
+
+//9- Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2.
+let divisao = []
+
+for (let i = 0; i < array.length; i += 1) {
+    divisao.push(array[i] / 2)
+} 
+console.log(divisao);
