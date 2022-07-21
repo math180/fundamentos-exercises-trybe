561db4726620dac71888745c38dc59a04dca830a
Matheus <matheusaraujo.lopes02092016@gmail.com>
2022-07-21 19:11:46 -0300

    exercicio 8

--- script.js
+++ script.js
@@ -77,3 +77,10 @@ for (let i = 1; i < numbers5.length; i += 1) {
     }
 } console.log(menorNumero);
 
+//8- Utilizando for, crie um array que vá de 1 até 25 e imprima o resultado;
+let array = []
+
+for (let i = 0; i <= 25; i += 1) {
+    array.push(i)
+} 
+    console.log(array);
