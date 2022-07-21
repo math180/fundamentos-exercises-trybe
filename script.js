0e43aec51c5cdecd723a77b835438d69fa0e831f
Matheus <matheusaraujo.lopes02092016@gmail.com>
2022-07-21 18:34:49 -0300

    exercicio 3

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ for (let i = 0; i < numbers.length; i += 1) {
   console.log(numbers[i]); 
 }
 
-// 2- Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
+// 2- some todos os valores contidos no array e imprima o resultado;
+
 let numbers1 = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 let somaDosNumeros = 0
@@ -16,3 +17,15 @@ for (let i = 0; i < numbers1.length; i += 1) {
 }
 
 console.log(somaDosNumeros);
+
+//3- calcule e imprima a média aritmética dos valores contidos no array;
+
+let numbers2 = [30, 8, 4, 20, 70, 10, 100, 25, 35, 30];
+
+let mediaAritmetica = 0;
+
+for (let i = 0; i < numbers2.length; i += 1) {
+    mediaAritmetica += numbers2[i] / numbers2.length
+}
+
+console.log(mediaAritmetica);
