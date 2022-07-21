fa6d734fa5e6ee0719eeeeed7673af80dae75aef
Matheus <matheusaraujo.lopes02092016@gmail.com>
2022-07-21 16:01:51 -0300

    exercicio 2

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 for (let i = 0; i < numbers.length; i += 1) {
   console.log(numbers[i]); 
 }
+
+// 2- Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
+let numbers1 = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let somaDosNumeros = 0
+
+for (let i = 0; i < numbers1.length; i += 1) {
+  somaDosNumeros += numbers1[i]
+}
+
+console.log(somaDosNumeros);
