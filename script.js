57064a8437ed55efff3dc18f9a0fad4eb546fad3
Matheus <matheusaraujo.lopes02092016@gmail.com>
2022-07-23 21:33:55 -0300

    exercicio 2

--- script.js
+++ script.js
@@ -10,3 +10,13 @@ let multiplier = 1;
 for (let i = 0; fatorial > i; i += 1) {
   multiplier *= (fatorial - i)
 } console.log(multiplier);
+
+/* 2- Agora, desenvolva um algoritmo que é capaz de inverter uma palavra. Por exemplo, a palavra "banana" seria invertida para "ananab". Utilize a string abaixo como exemplo, depois troque por outras para verificar se seu algoritmo está funcionando corretamente. */
+
+let word = 'tryber';
+let wordInvert = '';
+
+for (let i = word.length-1; i >= 0; i -= 1) {
+  wordInvert += word[i]
+}
+console.log(wordInvert);
