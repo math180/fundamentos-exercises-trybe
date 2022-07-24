9bb12bff93861127fe29cce132161628d98a20e0
Matheus <matheusaraujo.lopes02092016@gmail.com>
2022-07-23 21:42:21 -0300

    exercicio 3

--- script.js
+++ script.js
@@ -20,3 +20,23 @@ for (let i = word.length-1; i >= 0; i -= 1) {
   wordInvert += word[i]
 }
 console.log(wordInvert);
+
+// 3- Escreva dois algoritmos: um que retorne a maior palavra deste array e outro que retorne a menor. Considere o número de caracteres de cada palavra.
+
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+
+let biggestWord = array[0];
+let smallestWord = array[0];
+
+for (let i = 0; i < array.length; i += 1) {
+    if (array[i].length > biggestWord.length) {
+        biggestWord = array[i]
+    };
+
+    if (array[i].length < smallestWord.length) {
+    smallestWord = array[i]
+  }
+};
+
+console.log(`The smallest word is: ${smallestWord}`);
+console.log(`The biggest word is: ${biggestWord}`);
