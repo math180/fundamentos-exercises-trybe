66273f3d9fc6d167c09dbda1ad141517835db554
Matheus <matheusaraujo.lopes02092016@gmail.com>
2022-07-25 19:08:54 -0300

    exercicio 2

--- script.js
+++ script.js
@@ -8,4 +8,9 @@ let info = {
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
   };
 
-console.log(`Boas vindas, ${info.personagem}`);
+console.log(`Boas vindas, ${info.personagem}`)
+
+//🚀 2 - Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console. Valor esperado no console:
+
+info.recorrente = 'Sim'
+console.log(info);
