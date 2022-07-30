3482162e67f8494f14333ddbec15e71b1867a77b
Matheus <matheusaraujo.lopes02092016@gmail.com>
2022-07-30 18:01:31 -0300

    exercicio 7

--- script.js
+++ script.js
@@ -197,3 +197,24 @@ function countNumbers(naturalNumber) {
   }
   return sum; 
 } console.log(countNumbers(10));
+
+//7 - Crie uma função que receba uma string word e outra string ending. Verifique se a string ending é o final da string word. Considere que a string ending sempre será menor que a string word.
+
+  function verifyEndString(word, ending) {
+    word = word.split('');
+    ending = ending.split('');
+    let controle = true;
+  
+    for (let index = 0; index < ending.length; index += 1) {
+      if (word[word.length - ending.length + index] != ending[index]) {
+        controle = false;
+      }
+    }
+  
+    return controle;
+  }
+  
+  console.log(verifyEndString('trybe', 'be'));
+  console.log(verifyEndString('joaofernando', 'fernan'));
+
+
