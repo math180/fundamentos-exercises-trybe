6ef4859d65a0f841dbf37d42d4823f8e17d1d6d8
Matheus <matheusaraujo.lopes02092016@gmail.com>
2022-08-06 19:40:39 -0300

    adicionando evento no botão limpar

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
-let moment = require('moment');
 
-let age = moment('02092002', 'DDMMYYYY');
-let fromNow = age.fromNow();
-
-console.log('A sua idade atual é de:' + fromNow);
 
 const getButton = document.querySelector('#submit-btn');
-const getINputName = document.querySelector('#')
+const getBtnClear = document.querySelector('#clear-btn');
+
+const getInputName = document.querySelector('#inputName');
+const getInputEmail = document.querySelector('#inputEmail');
+const getCheckBox = document.querySelectorAll('.form-check-input');
+const getTextArea = document.querySelector('#floatingTextarea2');
+const getInputDate = document.querySelector('#input-date');
+const getInputCheck = document.querySelector('#exampleCheck1');
+const getCheckBox2 = document.querySelector('#checkbox2');
+
+
+function getNameValue(event) {
+    event.preventDefault();
+    console.log(getInputName.value);
+}
+getButton.addEventListener('click', getNameValue);
+
+function clearValues() {
+    getInputName.value = '';
+    getInputEmail.value = '';
+    for(let i = 0; i < getCheckBox.length; i += 1) {
+        getCheckBox[i].checked = 0
+    }
+    getTextArea.value = '';
+    getInputDate.value = '';
+} 
+
+getBtnClear.addEventListener('click', clearValues);
