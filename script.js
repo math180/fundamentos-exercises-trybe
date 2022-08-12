a3c29baea86a23e92359835bf5927c3fb31d2d12
Matheus <matheusaraujo.lopes02092016@gmail.com>
2022-08-12 18:58:16 -0300

    exercício 1

---
+++ script.js
@@ -0,0 +1,19 @@
+/* 1- 🚀 Altere o tipo das variáveis(var) para let ou const para que respeitem o escopo em que foram declaradas, e altere a função utilizando arrow function e template literals.
+Modifique a estrutura da função para que ela seja uma arrow function;
+Modifique as variáveis para que respeitem o escopo onde estão declaradas;
+Modifique as concatenações para template literals.
+ */
+
+function testingScope(escopo) {
+    if (escopo === true) {
+      let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+      ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+      console.log(ifScope);
+    } else {
+      let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+      console.log(elseScope);
+    }
+    console.log(ifScope + ' o que estou fazendo aqui ? :O');
+  }
+
+  testingScope(true);
