9514040876161e7445764c378b5ea5a4981e95dc
Matheus <matheusaraujo.lopes02092016@gmail.com>
2022-08-23 23:04:40 -0300

    exercicio 3

--- script.js
+++ script.js
@@ -1,82 +1,87 @@
 const books = [
-    {
-        id: 1,
-        name: 'As Crônicas de Gelo e Fogo',
-        genre: 'Fantasia',
-        author: {
-            name: 'George R. R. Martin',
-            birthYear: 1948,
-        },
-        releaseYear: 1991,
+  {
+    id: 1,
+    name: "As Crônicas de Gelo e Fogo",
+    genre: "Fantasia",
+    author: {
+      name: "George R. R. Martin",
+      birthYear: 1948,
     },
-    {
-        id: 2,
-        name: 'O Senhor dos Anéis',
-        genre: 'Fantasia',
-        author: {
-            name: 'J. R. R. Tolkien',
-            birthYear: 1892,
-        },
-        releaseYear: 1954,
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: "O Senhor dos Anéis",
+    genre: "Fantasia",
+    author: {
+      name: "J. R. R. Tolkien",
+      birthYear: 1892,
     },
-    {
-        id: 3,
-        name: 'Fundação',
-        genre: 'Ficção Científica',
-        author: {
-            name: 'Isaac Asimov',
-            birthYear: 1920,
-        },
-        releaseYear: 1951,
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: "Fundação",
+    genre: "Ficção Científica",
+    author: {
+      name: "Isaac Asimov",
+      birthYear: 1920,
     },
-    {
-        id: 4,
-        name: 'Duna',
-        genre: 'Ficção Científica',
-        author: {
-            name: 'Frank Herbert',
-            birthYear: 1920,
-        },
-        releaseYear: 1965,
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: "Duna",
+    genre: "Ficção Científica",
+    author: {
+      name: "Frank Herbert",
+      birthYear: 1920,
     },
-    {
-        id: 5,
-        name: 'A Coisa',
-        genre: 'Terror',
-        author: {
-            name: 'Stephen King',
-            birthYear: 1947,
-        },
-        releaseYear: 1986,
+    releaseYear: 1965,
+  },
+  {
+    id: 5,
+    name: "A Coisa",
+    genre: "Terror",
+    author: {
+      name: "Stephen King",
+      birthYear: 1947,
     },
-    {
-        id: 6,
-        name: 'O Chamado de Cthulhu',
-        genre: 'Terror',
-        author: {
-            name: 'H. P. Lovecraft',
-            birthYear: 1890,
-        },
-        releaseYear: 1928,
+    releaseYear: 1986,
+  },
+  {
+    id: 6,
+    name: "O Chamado de Cthulhu",
+    genre: "Terror",
+    author: {
+      name: "H. P. Lovecraft",
+      birthYear: 1890,
     },
-]
+    releaseYear: 1928,
+  },
+];
 
 // 🚀 1 - Crie um array com strings no formato NOME_DO_LIVRO - GÊNERO_DO_LIVRO - NOME_DA_PESSOA_AUTORA
 
 const newArr = () =>
-    books.map((book) => `${book.name} - ${book.genre} - ${book.author.name}`)
+  books.map((book) => `${book.name} - ${book.genre} - ${book.author.name}`);
 
-console.log(newArr())
+console.log(newArr());
 
 // 🚀 2 - Construa um array de objetos a partir do array de livros. Cada objeto deve conter uma propriedade author, com o nome da pessoa autora do livro, e uma propriedade age com a idade dessa pessoa quando o livro foi lançado. O array deve ser ordenado por idade, ou seja, da pessoa mais jovem para a mais velha considerando suas idades quando o livro foi lançado.
 
 const newObject = () => {
-    return books
-        .map((book) => ({
-            author: book.author.name,
-            age: book.releaseYear - book.author.birthYear,
-        }))
-        .sort((item1, item2) => item1.age - item2.age)
-}
+  return books.map((book) => ({
+      author: book.author.name,
+      age: book.releaseYear - book.author.birthYear,
+    }))
+    .sort((item1, item2) => item1.age - item2.age);
+};
 
-console.log(newObject())
+console.log(newObject());
+
+// 3 - Crie um array com todos os objetos que possuem gênero ficção científica ou fantasia.
+
+const everyGenre = () => books.filter((book) => book.genre === 'Fantasia' || book.genre === 'Ficção Científica')
+
+console.log(everyGenre());
