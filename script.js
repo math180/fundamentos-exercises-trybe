33f7bf60a7c9628a37b94a216ad1b8735d711647
Matheus <matheusaraujo.lopes02092016@gmail.com>
2022-08-24 23:01:34 -0300

    exercicio 5

--- script.js
+++ script.js
@@ -242,4 +242,22 @@ const biggestBookk = (array) => {
  
  console.log(biggestBookk(books)); */
 
+ //🚀 5- Dada o array de nomes, retorne a quantidade de vezes em que aparecem a letra a maiúscula ou minúscula.
+
+ const names = [
+    'Aanemarie', 'Adervandes', 'Akifusa',
+    'Abegildo', 'Adicellia', 'Aladonata',
+    'Abeladerco', 'Adieidy', 'Alarucha',
+  ];
+  
+const namesQtd = (array) => {
+   return array.reduce((acc, curr) => 
+      acc + curr.split('').reduce((acumulator, current) => {
+        if (current === 'a' || current === 'A') return acumulator + 1;
+        return acumulator
+      }, 0), 0);
+}
+
+ console.log(namesQtd(names));
+
  
