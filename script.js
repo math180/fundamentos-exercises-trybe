e0b4512dd57c976e484044fe44c0dd65c7bbf07d
Matheus <matheusaraujo.lopes02092016@gmail.com>
2022-08-25 22:33:59 -0300

    exercicio 1

---
+++ script.js
@@ -0,0 +1,27 @@
+// Para fixar e praticar, vamos fazer uma salada de frutas com itens adicionais que você desejar. Faça uma função chamada fruitSalad passando como parâmetro specialFruit e additionalItens, faça a função retornar uma lista única contendo todos os itens da nossa salada de frutas usando o spread.
+
+// Faça uma lista com as suas frutas favoritas
+const specialFruit = ['Mango', 'Avocado', 'Pineapple'];
+
+// Faça uma lista de complementos que você gostaria de adicionar
+const additionalItens = ['Banana', 'Apricot', 'Grape'];
+
+const fruitSalad = (fruit, additional) => {
+  const spreadUsed = [...fruit, ...additional]
+  return spreadUsed
+};
+
+console.log(fruitSalad(specialFruit, additionalItens));
+
+// 1 - Dado o código abaixo, altere a chamada da função rectangleArea de forma que seja impressa a área dos 3 retângulos (rectangle1, rectangle2 e rectangle3). O código deve retornar em sequência 2, 15 e 54.
+
+const rectangleArea = (width, height) => width * height;
+
+const rectangle1 = [1, 2];
+const rectangle2 = [3, 5];
+const rectangle3 = [6, 9];
+const rectangles = [rectangle1, rectangle2, rectangle3];
+
+rectangles.forEach((rectangle) => {
+  console.log(rectangleArea(...rectangle));
+})
